6b4406b1a0f4de4d4081dd4382676fd1fc2faaa1
Cameron Sinko <csinko@gmail.com>
2017-02-11 15:25:37 -0600

    File Download Working

--- local-server/index.js
+++ local-server/index.js
@@ -2,20 +2,22 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
+var fileSystem = require('fs');
+var path = require('path');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
-// POST http://localhost:8080/api/users
-// parameters sent with 
-app.post('/api/users', function(req, res) {
-	var user_id = req.headers.id;
-	var token = req.headers.token;
-	var geo = req.headers.geo
-	console.log(req.headers.id);
-	res.send(user_id + ' ' + token + ' ' + geo);
+
+app.get('/application', function(req, res) {
+	var filePath= path.join(__dirname, 'test.txt');
+	var stat = fileSystem.statSync(filePath);
+	res.writeHead(200, {'Content-Type': 'application/octet-stream'});
+	var readStream = fileSystem.createReadStream(filePath);
+	readStream.pipe(res);
+
 });
 
 // start the server
