591f413f0e4b30ff5cce85a54f385504bbecb38b
emersoncloud <emerson.cloud@gmail.com>
2017-02-12 04:11:33 -0500

    fixed add endpoitns

--- local-server/index.js
+++ local-server/index.js
@@ -13,6 +13,8 @@ app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+remote_server_ip = 'http://40.71.25.155:8080'
+
 // software list
 var files = {"software": [{"clean_name": "Android Studio", "id" : 1, "name": "android-studio-ide-145.3360264-linux.zip"}, {"clean_name": "JRE 1.8", "id":2 ,"name": "jre-8u121-linux-i586.tar.gz"} , {"clean_name": "Postman", "id":3 ,"name":"Postman-linux-x64-4.9.3.tar.gz"} , {"clean_name": "AuthPy", "id":4 ,"name":"authy-authy-python-f085687.zip"} , {"clean_name": "MonoDevelop", "id":5 ,"name":"monodevelop-6.1.2.44-1.flatpak"} , {"clean_name": "SimpleSMS", "id":6,"name":"simpleSMS-master.zip"} , {"clean_name": "Ngrok x64", "id":7 ,"name":"ngrok-stable-linux-amd64.zip"} , {"clean_name": "Java OCR", "id":8 ,"name":"javaocr-20100605.zip"} , {"clean_name": "JDK 1.8", "id":9 ,"name":"jdk-8u121-linux-i586.tar.gz"} ]};
 	
@@ -24,7 +26,7 @@ var filename = 'test.txt';
 //initialize server information with remote
 //console.log("running");
 request.get({
-	url:'http://software-lab.azurewebsites.net/init_node',
+	url: remote_server_ip + '/init_node',
 	json: {
 		'name': 'node',
 		'private_ip':'10.2.0.252'
@@ -36,42 +38,44 @@ request.get({
 
   //in that case the body will have a target_ip from which to fetch all
   //files
-	console.log(body);
-  console.log(body.name);
-  console.log(body.target_ip);
+	//console.log(body);
+  //console.log(body.name);
+  //target_ip = console.log(body.target_ip);
 
-  //conect to and download all from 'target_ip'
-  //with cam's shit
+  ////conect to and download all from 'target_ip'
+  ////with cam's shit
 
-  for (i=1; i < 10; i++) {
-    request.get({
-      url:'https://' + target_ip + ':8080/application?id=' + i.toString() 
-    }, function(err, httpResponse, body){
-	    if(err){
-	    	console.log('Error in getting all files', err);
-	    	return;
-	    }
-    });
-  }
+  //for (i=1; i < 10; i++) {
+  //  console.log("RUNNING HERE");
+  //  request.get({
+  //    url:'https://' + target_ip + ':8080/application?id=' + i.toString() 
+  //  }, function(err, httpResponse, body){
+	//    if(err){
+	//    	console.log('Error in getting all files', err);
+	//    	return;
+	//    }
+  //  });
+  //}
 
   //send something else back to azure saying update the private_ip of this
-  request.post({
-  	url:'http://software-lab.azurewebsites.net/register_node',
-    json: {
-      'name': 'Brick Hack',
-      'private_ip': '10.2.0.252'
-    }
-  }, function(err, httpResponse, body) {
-    if(err) {
-      console.log('Error in register_node from node');
-      return;
-    }
-  });
+  //console.log("just got here");
+  //request.post({
+  //	url: remote_server_ip + '/register_node',
+  //  json: {
+  //    'name': 'Brick Hack',
+  //    'ip': '10.2.0.252'
+  //  }
+  //}, function(err, httpResponse, body) {
+  //  if(err) {
+  //    console.log('Error in register_node from node');
+  //    return;
+  //  }
+  //});
 
-	if(err){
-		console.log('Error in init_node', err);
-		return;
-	}
+	//if(err){
+	//	console.log('Error in init_node', err);
+	//	return;
+	//}
 });
 
 console.log("sent posts");
@@ -81,8 +85,9 @@ app.get('/test', function(req, res) {
 });
 
 app.get('/application', function(req, res) {
+    console.log("got request!!!!!!");
     request.post({
-    	url:'http://software-lab.azurewebsites.net/add_downloading_node',
+    	url: remote_server_ip + '/add_downloading_user',
       json: {
         'name': 'Brick Hack',
         'ip': '10.2.0.252'
@@ -107,7 +112,7 @@ app.get('/application', function(req, res) {
     console.log("FINISHED IT ALL");
 
     request.post({
-    	url:'http://software-lab.azurewebsites.net/subtract_downloading_node',
+    	url: remote_server_ip + '/subtract_downloading_user',
       json: {
         'name': 'Brick Hack',
         'ip': '10.2.0.252'
