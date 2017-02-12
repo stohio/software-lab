6de08e9c04db0e0e257578bf510dd91e10fd7056
Cameron Sinko <csinko@gmail.com>
2017-02-11 18:58:04 -0600

    added basic download function

--- local-server/index.js
+++ local-server/index.js
@@ -4,17 +4,32 @@ var app = express();
 var port = process.env.PORT || 8080;
 var fileSystem = require('fs');
 var path = require('path');
+var http = require('http');
 
 var bodyParser = require('body-parser');
+
+app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+// placeholder filename
+var filename = 'test.txt';
 
+var options = {
+	name: 'Brick Hack',
+	private_ip: '10.2.1.91',
+	software: []
+	
+};
 
+// initialize server information with remote
 app.get('/application', function(req, res) {
-	var filePath= path.join(__dirname, 'test.txt');
+	var filePath= path.join(__dirname, filename);
 	var stat = fileSystem.statSync(filePath);
-	res.writeHead(200, {'Content-Type': 'application/octet-stream'});
+	res.writeHead(200, {
+		'Content-Type': 'application/octet-stream',
+		'Content-Disposition': 'attachment; filename=' + filename
+	});
 	var readStream = fileSystem.createReadStream(filePath);
 	readStream.pipe(res);
 
