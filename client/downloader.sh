fa5bd26cbb57c95066661f2c27c75c29433c0ae5
Cameron Sinko <csinko@gmail.com>
2017-02-12 02:24:44 -0500

    script

--- client/downloader.sh
+++ client/downloader.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+#Request a local server IP
+#server_IP= curl -X GET -s 'software-lab.azurewebsites.net/server'
+server_IP="10.1.26.45"
+
+echo 'curl -x GET "$server_IP:8080/application?id=1" -o test.tmp -w "%{speed_download}"'
+
 
