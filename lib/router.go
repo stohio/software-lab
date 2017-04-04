13c2cfc9e61766944bce7c4f597c3f5fe275acd6
Matthew Roseman <mroseman95@gmail.com>
2017-04-04 15:34:02 -0400

    Also moved the NewRouter function to lib and it gets the routes struct passed into it directly
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

---
+++ lib/router.go
@@ -0,0 +1,25 @@
+package softwarelab
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// NewRouter creates a new router that maps all routes in routes to their respective handler function
+func NewRouter(routes Routes) *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		var handler http.Handler
+
+		handler = route.HandlerFunc
+		handler = RouteLogger(handler, route.Name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+	}
+	return router
+}
--- remote/main.go
+++ remote/main.go
@@ -8,7 +8,7 @@ import (
 
 // main creates a new router instance and starts running the http server on port 8080
 func main() {
-	router := NewRouter()
+	router := swl.NewRouter(routes)
 	swl.InitLogger()
 
 	swl.ConsoleLog.Printf("Remote Server is Running")
