ae8aaa9fdd140719134c56c11b7e62fe1b2e9264
Matthew Roseman <mroseman95@gmail.com>
2017-05-27 17:10:48 -0400

    removed website ip and port number from config
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- lib/config.go
+++ lib/config.go
@@ -11,12 +11,10 @@ import (
 // TODO for now there will be just one NetworkSetup, but
 // for the future there could be a development and production
 type NetworkSetup struct {
-	WebsiteServer string `json:"website_server"`
-	WebsitePort   string `json:"website_port"`
-	RemoteServer  string `json:"remote_server"`
-	RemotePort    string `json:"remote_port"`
-	LocalServer   string `json:"local_server"`
-	LocalPort     string `json:"local_port"`
+	RemoteServer string `json:"remote_server"`
+	RemotePort   string `json:"remote_port"`
+	LocalServer  string `json:"local_server"`
+	LocalPort    string `json:"local_port"`
 }
 
 // GetNetworkSettings returns a NetworkSetup struct detailing the
@@ -33,16 +31,6 @@ func GetNetworkSettings() NetworkSetup {
 	}
 
 	return config
-}
-
-// GetWebsiteServer returns the software labs website's IP address
-func GetWebsiteServer() string {
-	return GetNetworkSettings().WebsiteServer
-}
-
-// GetWebsitePort returns the software labs website's port number
-func GetWebsitePort() string {
-	return GetNetworkSettings().WebsitePort
 }
 
 // GetRemoteServer returns the software labs remote server' IP address
