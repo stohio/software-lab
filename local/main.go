85878f0477b791dc07d9da654db34816ea8d2524
Matthew Roseman <mroseman95@gmail.com>
2017-05-29 18:46:10 -0400

    deleted the commented out sleep function call
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- local/main.go
+++ local/main.go
@@ -240,7 +240,6 @@ func CheckOrDownload(softwares swl.Softwares, initial bool) {
 		for _, v := range s.Versions {
 			filename := path + "/" + strconv.Itoa(v.ID) + v.Extension
 			if _, err := os.Stat(filename); os.IsNotExist(err) {
-				//time.Sleep(time.Second * 2)
 				if initial {
 					fmt.Printf("Downloading %s - %s ...\n", s.Name, v.OS)
 					err := DownloadFromRemote(s, v, filename)
